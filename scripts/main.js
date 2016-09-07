ed95048d78bbfe4c61fe9288cbdb623adcc32a18
Natalia Malkovska <natalia.malkovska@gmail.com>
2016-09-07 14:25:27 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// Set current  year in the footer:
+var date = document.querySelector('.date'),
+    currentYear = new Date().getFullYear();
+date.innerText = currentYear;
+
+/*// Set current li - unnecessary:
+ var currentLi = document.querySelector('.current'),
+ nav = document.querySelector('.main-nav');
+ nav.addEventListener('click', function (ev) {
+ if (ev.target.nodeName == 'LI'){
+ currentLi.classList.remove('current');
+ ev.target.classList.add('current');
+ }
+ }, false);*/
+
+// Create image gallery:
+var currentImg = document.querySelector('#gallery-current'),
+    smallGal = document.querySelector('.small-gal');
+smallGal.addEventListener('click', function (ev) {
+    if (ev.target.nodeName == 'IMG') {
+        currentImg.src = ev.target.src;
+    }
+}, false);
