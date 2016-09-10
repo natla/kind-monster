8a32b957fdb4ec7a8080daf88e3e88c008afe788
natla <natalia.malkovska@gmail.com>
2016-09-10 15:50:28 +0300

    Edited main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,63 @@
+window.onload = function() {
 // Set current  year in the footer:
-var date = document.querySelector('.date'),
-    currentYear = new Date().getFullYear();
-date.innerText = currentYear;
+    var date = document.querySelector('.date');
+    date.innerText = new Date().getFullYear();
 
-/*// Set current li - unnecessary:
- var currentLi = document.querySelector('.current'),
- nav = document.querySelector('.main-nav');
- nav.addEventListener('click', function (ev) {
- if (ev.target.nodeName == 'LI'){
- currentLi.classList.remove('current');
- ev.target.classList.add('current');
- }
- }, false);*/
+    /*// Set current li - unnecessary:
+     var currentLi = document.querySelector('.current'),
+     nav = document.querySelector('.main-nav');
+     nav.addEventListener('click', function (ev) {
+     if (ev.target.nodeName == 'LI'){
+     currentLi.classList.remove('current');
+     ev.target.classList.add('current');
+     }
+     }, false);*/
 
 // Create image gallery:
-var currentImg = document.querySelector('#gallery-current'),
-    smallGal = document.querySelector('.small-gal');
-if (smallGal) {
-    smallGal.addEventListener('click', function (ev) {
-        if (ev.target.nodeName == 'IMG') {
-            currentImg.src = ev.target.src;
-        }
-    }, false);
-}
+    var currentImg = document.querySelector('#gallery-current'),
+        smallGal = document.querySelector('.small-gal');
+    if (smallGal) {
+        smallGal.addEventListener('click', function (ev) {
+            if (ev.target.nodeName === 'IMG') {
+                currentImg.src = ev.target.src;
+            }
+        }, false);
+    }
 
 // Off-canvas nav menu for smaller screens (under 768px):
-var menuIcon = document.querySelector('#menu-icon'),
-    plusIcon = document.querySelector('#plus-icon'),
-    minusIcon = document.querySelector('#minus-icon'),
-    drawer = document.querySelector('.main-nav'),
-    innerList = document.querySelector('.inner-nav');
+    var menuIcon = document.querySelector('#menu-icon'),
+        plusIcon = document.querySelector('#plus-icon'),
+        minusIcon = document.querySelector('#minus-icon'),
+        drawer = document.querySelector('.main-nav'),
+        innerList = document.querySelector('.inner-nav');
 
-menuIcon.addEventListener('click', function (ev) {
-    drawer.classList.toggle('open');
-    ev.stopPropagation();
-}, false);
+    menuIcon.addEventListener('click', function (ev) {
+        drawer.classList.toggle('open');
+        ev.stopPropagation();
+    }, false);
 
 // Hovering the plus icon or its parent element removes the hover effect from the list items:
-plusIcon.addEventListener('mouseover', function(ev){
-}, false);
-plusIcon.parentElement.addEventListener('mouseover', function(ev){
-    innerList.parentElement.classList.add('no-hover');
-}, false);
+    plusIcon.addEventListener('mouseover', function (ev) {
+        innerList.parentElement.classList.add('no-hover');
+        ev.stopPropagation();
+    }, false);
+
+    plusIcon.parentElement.addEventListener('mouseover', function (ev) {
+        if (window.getComputedStyle(plusIcon, null).display === 'block') {
+            innerList.parentElement.classList.add('no-hover');
+        }
+        else {
+            innerList.parentElement.classList.remove('no-hover');
+        }
+         ev.stopPropagation();
+    }, false);
 
 
 // Clicking the plus icon open the inner menu, clicking the minus icon closes the inner menu:
-plusIcon.addEventListener('click', function (ev) {
-    innerList.classList.toggle('open');
-    this.classList.toggle('rotate');
-    minusIcon.classList.toggle('rotate');
-    ev.stopPropagation();
-}, false);
+    plusIcon.addEventListener('click', function (ev) {
+        innerList.classList.toggle('open');
+        this.classList.toggle('rotate');
+        minusIcon.classList.toggle('rotate');
+        ev.stopPropagation();
+    }, false);
+};
