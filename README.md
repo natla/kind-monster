f127a6acd7f2bb4bff5437eda1bf4cf9ae9f4414
natla <natalia.malkovska@gmail.com>
2016-09-10 15:14:29 +0300

    Implemented responsive off-canvas nav menu

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ Website design project for an online boutique selling handmade dolls.
 This website is a work in progress, its design gets updated from time to time.
 Since it's made for designs purposes, it lacks commercial functionality (e.g., the "Add to Cart" button doesn't actually add anything to the cart, which stays empty at all times).
 
+The social share icons have fake functionality, sharing pages of a non-existing website.
+
 The website can be seen here: https://natla.github.io/kind-monster/ 
 
 All the dolls are hand crocheted by me and their photos are also taken by me (crocheting amigurumis is one of my hobbies).
+
+### TO DO:
+
+Change the natural resolution of the images to match their size on screen.
--- scripts/main.js
+++ scripts/main.js
@@ -16,11 +16,38 @@ date.innerText = currentYear;
 // Create image gallery:
 var currentImg = document.querySelector('#gallery-current'),
     smallGal = document.querySelector('.small-gal');
-	if (smallGal) {
-		smallGal.addEventListener('click', function (ev) {
-    if (ev.target.nodeName == 'IMG') {
-        currentImg.src = ev.target.src;
-    }
-}, false);
+if (smallGal) {
+    smallGal.addEventListener('click', function (ev) {
+        if (ev.target.nodeName == 'IMG') {
+            currentImg.src = ev.target.src;
+        }
+    }, false);
 }
 
+// Off-canvas nav menu for smaller screens (under 768px):
+var menuIcon = document.querySelector('#menu-icon'),
+    plusIcon = document.querySelector('#plus-icon'),
+    minusIcon = document.querySelector('#minus-icon'),
+    drawer = document.querySelector('.main-nav'),
+    innerList = document.querySelector('.inner-nav');
+
+menuIcon.addEventListener('click', function (ev) {
+    drawer.classList.toggle('open');
+    ev.stopPropagation();
+}, false);
+
+// Hovering the plus icon or its parent element removes the hover effect from the list items:
+plusIcon.addEventListener('mouseover', function(ev){
+}, false);
+plusIcon.parentElement.addEventListener('mouseover', function(ev){
+    innerList.parentElement.classList.add('no-hover');
+}, false);
+
+
+// Clicking the plus icon open the inner menu, clicking the minus icon closes the inner menu:
+plusIcon.addEventListener('click', function (ev) {
+    innerList.classList.toggle('open');
+    this.classList.toggle('rotate');
+    minusIcon.classList.toggle('rotate');
+    ev.stopPropagation();
+}, false);
